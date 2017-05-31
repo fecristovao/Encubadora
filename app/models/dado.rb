b31a7fb7092efe00b91a54bcbc507b11591c0823
Fernando Cristovao <fe.acristovao@gmail.com>
2017-05-31 06:22:22 +0000

    Jobs

--- app/models/dado.rb
+++ app/models/dado.rb
@@ -1,12 +1,17 @@
-require 'open-uri'
+require 'net/http'
 
 class Dado < ApplicationRecord
 	def self.get_dados
-		pagina = open("http://casardo.ddns.net:8080").read
+		url = URI.parse('http://casardo.ddns.net:8080')
+		pagina = Net::HTTP.get(url)
+
 
 		pagina = pagina.scan(/<br\/>Atual: (.*?)<br\/>/)
+
+				
 		pagina[0] = pagina[0].to_s.gsub!('["', "").gsub!('"]', "").gsub(" C", "")
 		pagina[1] = pagina[1].to_s.gsub!('["', "").gsub!('"]', "").gsub(" %", "")
+
 
 		novo = Dado.new
 		novo.temperatura = pagina[0]
--- teste.rb
+++ teste.rb
@@ -1,8 +1,11 @@
-require 'open-uri'
+require 'net/http'
 
-pagina = open("http://casardo.ddns.net:8080").read
+url = URI.parse('http://casardo.ddns.net:8080')
+pagina = Net::HTTP.get(url)
+
+
 pagina = pagina.scan(/<br\/>Atual: (.*?)<br\/>/)
+
 		
 pagina[0] = pagina[0].to_s.gsub!('["', "").gsub!('"]', "").gsub(" C", "")
 pagina[1] = pagina[1].to_s.gsub!('["', "").gsub!('"]', "").gsub(" %", "")
-puts pagina[0]
