8d98ed1125253c33a3b200efc84e35d2ef8521c1
Fernando Cristovao <fe.acristovao@gmail.com>
2017-05-31 03:55:09 +0000

    Min, max e medias

--- app/controllers/controlador_controller.rb
+++ app/controllers/controlador_controller.rb
@@ -2,6 +2,39 @@ class ControladorController < ApplicationController
 	def list
 		#Dado.get_dados
 		@dados = Dado.all
+		@atr = {
+			:max_t => 0,
+			:min_t => 999,
+			:max_u => 0,
+			:min_u => 999,
+			:media_t => 0,
+			:media_u => 0
+		}
+
+		@dados.each do |dado|
+			if @atr[:max_t] <= dado.temperatura
+				@atr[:max_t] = dado.temperatura
+			end
+			
+			if @atr[:max_u] <= dado.umidade
+				@atr[:max_u] = dado.umidade
+			end
+			
+			if @atr[:min_t] >= dado.temperatura
+				@atr[:min_t] = dado.temperatura
+			end
+
+			if @atr[:min_u] >= dado.umidade
+				@atr[:min_u] = dado.umidade
+			end
+
+			@atr[:media_u] = @atr[:media_u] + dado.umidade
+			@atr[:media_t] = @atr[:media_t] + dado.temperatura
+
+		end
+			@atr[:media_u] = @atr[:media_u]/@dados.count
+			@atr[:media_t] = @atr[:media_t]/@dados.count
+
 
 	end
 end
